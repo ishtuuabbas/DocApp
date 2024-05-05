dcd55044b45ee11ae02ed666ae2a946301dcb1cf
Ishubalti <ishtuuabbas786@gmail.com>
2024-05-05 14:11:22 +0500

    service file change

--- fypfrontend/src/components/Service/Service.jsx
+++ fypfrontend/src/components/Service/Service.jsx
@@ -5,6 +5,9 @@ import Header from '../Shared/Header/Header'
 import img from '../../images/features/baby.png'
 import img1 from '../../images/features/img1.jpg'
 import img2 from '../../images/features/img2.jpg'
+import img4 from '../../images/features/img3.jpg'
+import img5 from '../../images/features/img5.jpg'
+import img6 from '../../images/features/img4.jpg'
 
 import { Link } from 'react-router-dom'
 import doctorBg from '../../images/img/doctors-bg.jpg';
@@ -28,9 +31,8 @@ const Service = () => {
 
       <div className="container" style={{ marginTop: 200, marginBottom: 100 }}>
         <div className="row">
-          {
-            Array(3).fill(null).map((_item, id) => (
-              <div className="col-lg-4 col-md-6 col-sm-6" key={id + 3}>
+          
+              <div className="col-lg-4 col-md-6 col-sm-6" >
                 <div className="card shadow border-0 mb-5">
                   <img src={img} alt="" className="img-fluid" style={{ maxHeight: '17rem', objectFit: 'cover' }} />
                   <div className="p-2">
@@ -38,6 +40,8 @@ const Service = () => {
                     <p className="mb-4">Saepe nulla praesentium eaque omnis perferendis a doloremque.</p>
                   </div>
                 </div>
+                </div>
+                <div className="col-lg-4 col-md-6 col-sm-6" >
                 <div className="card shadow border-0 mb-5">
                   <img src={img1} alt="" className="img-fluid" style={{ maxHeight: '17rem', objectFit: 'cover' }} />
                   <div className="p-2">
@@ -45,6 +49,8 @@ const Service = () => {
                     <p className="mb-4">Saepe nulla praesentium eaque omnis perferendis a doloremque.</p>
                   </div>
                 </div>
+                </div>
+                <div className="col-lg-4 col-md-6 col-sm-6" >
                 <div className="card shadow border-0 mb-5">
                   <img src={img2} alt="" className="img-fluid" style={{ maxHeight: '17rem', objectFit: 'cover' }} />
                   <div className="p-2">
@@ -53,8 +59,33 @@ const Service = () => {
                   </div>
                 </div>
               </div>
-            ))
-          }
+              <div className="col-lg-4 col-md-6 col-sm-6" >
+                <div className="card shadow border-0 mb-5">
+                  <img src={img4} alt="" className="img-fluid" style={{ maxHeight: '17rem', objectFit: 'cover' }} />
+                  <div className="p-2">
+                    <h4 className="mt-4 mb-2">X-ray care</h4>
+                    <p className="mb-4">Saepe nulla praesentium eaque omnis perferendis a doloremque.</p>
+                  </div>
+                </div>
+                </div>
+                <div className="col-lg-4 col-md-6 col-sm-6" >
+                <div className="card shadow border-0 mb-5">
+                  <img src={img5} alt="" className="img-fluid" style={{ maxHeight: '17rem', objectFit: 'cover' }} />
+                  <div className="p-2">
+                    <h4 className="mt-4 mb-2">BP care</h4>
+                    <p className="mb-4">Saepe nulla praesentium eaque omnis perferendis a doloremque.</p>
+                  </div>
+                </div>
+                </div>
+                <div className="col-lg-4 col-md-6 col-sm-6" >
+                <div className="card shadow border-0 mb-5">
+                  <img src={img6} alt="" className="img-fluid" style={{ maxHeight: '17rem', objectFit: 'cover' }} />
+                  <div className="p-2">
+                    <h4 className="mt-4 mb-2">Eye care</h4>
+                    <p className="mb-4">Saepe nulla praesentium eaque omnis perferendis a doloremque.</p>
+                  </div>
+                </div>
+              </div>
         </div>
       </div>
 
@@ -69,8 +100,9 @@ const Service = () => {
             <div className="col-lg-7">
               <div className="d-flex align-items-center">
                 <div className='mb-4 section-title text-center'>
-                  <h2 className='text-uppercase'>We are pleased to offer you the</h2>
-                  <p className='form-text'>We empower clinics and doctors with innovative solutions for streamlined operations, enhanced patient care, and secure data management.</p>
+                  <h2 className='text-uppercase'>We are ready to help your health problems</h2>
+                  <p className='form-text'> There are many things can be done to help relieve the symptoms of illness or injury,and improve recovery. Positive thinking , relaxation, a healthy diet, regular exercise, good 
+                  sleep and social support can all promote health and wellbeing.</p>
                   <Link to={'/doctors'} className="btn-get-started scrollto">Get Started</Link>
                 </div>
 
